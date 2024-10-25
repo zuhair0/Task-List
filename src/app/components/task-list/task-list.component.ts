b33b369378aac492401fdd707a60b9e022ab9702
Zuhair Saleem <zuhairsaleem343@gmail.com>
2024-10-25 19:49:10 +0500

    validation for forms

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
@@ -13,10 +14,24 @@ tasks:{name: string, createdAt: Date}[]=[];
 newTask:string =''
 showMessage:boolean=false
 emptyInput: boolean=false
-addTask(){
-  if(this.newTask.trim()){
+//---- Validation by own----//
+// addTask(){
+//   if(this.newTask==''){
+//     this.emptyText();
+//   }
+//   if(this.newTask.trim()){
+//     this.tasks.push({name: this.newTask, createdAt: new Date()})
+//     this.newTask=''
+//     this.emptyInput=false
+//   }
+// }
+
+//---- Validation ----//
+addTask(taskForm: any){
+  if(taskForm.valid){
     this.tasks.push({name: this.newTask, createdAt: new Date()})
     this.newTask=''
+    taskForm.reset()
   }
 }
 toggleMessage(){
