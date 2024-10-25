a8172e796bc9470ac08c423c9c3f4e0681f3feae
Zuhair Saleem <zuhairsaleem343@gmail.com>
2024-10-25 20:19:16 +0500

    reactive form validation

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-task-list',
@@ -26,13 +26,29 @@ emptyInput: boolean=false
 //   }
 // }
 
-//---- Validation ----//
-addTask(taskForm: any){
-  if(taskForm.valid){
-    this.tasks.push({name: this.newTask, createdAt: new Date()})
-    this.newTask=''
-    taskForm.reset()
-  }
+taskForm: FormGroup
+//in angular we use counstructor for dependance injection, initialiazation, auto executions
+constructor(){
+this.taskForm=new FormGroup({
+  taskName: new FormControl('',[Validators.required, Validators.minLength(3)])
+})
+}
+//---- Validation for template form----//
+// addTask(taskForm: any){
+//   if(taskForm.valid){
+//     this.tasks.push({name: this.newTask, createdAt: new Date()})
+//     this.newTask=''
+//     taskForm.reset()
+//   }
+// }
+
+//---- Validation for reactive form----//
+addTask(){
+if(this.taskForm.valid){
+  this.tasks.push({name: this.taskForm.value.taskName, createdAt: new Date()})
+  this.newTask=''
+  this.taskForm.reset()
+}
 }
 toggleMessage(){
   this.showMessage=!this.showMessage;
